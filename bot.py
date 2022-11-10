3aa550d92b006b783b7a1050cfa1375dfb8b6db0
Alexey <alexh2@yandex.ru>
2022-11-10 14:09:39 +0300

    hw10

---
+++ bot.py
@@ -0,0 +1,8 @@
+#Здесь создается бот и хранится его токен
+
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+
+bot = Bot(token='5677453204:AAG_woquRF2cHDock0FL2Q8LLh8GmKATAlY')
+dp = Dispatcher(bot)
